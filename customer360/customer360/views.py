3baeef7d829872bc4c2fdcdd7c2e9b31659081dd
HarbySmart <155396836+HarbySmart@users.noreply.github.com>
2024-08-31 00:37:40 +0100

    Update views.py

--- customer360/customer360/views.py
+++ customer360/customer360/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from datetime import date, timedelta
 from django.db.models import Count
-from .models import *
+from . models import *
 
 # Define index view
 def index(request):
@@ -25,7 +25,7 @@ def create_customer(request):
 # # Define summary view
 def summary(request):
     thirty_days_ago = date.today() - timedelta(days=30)
-    interactions = Interactions.objects.filter(interaction_date_gte=thirty_days_ago)
+    interactions = Interaction.objects.filter(interaction_date__gte=thirty_days_ago)
 
     count = len(interactions)
     interactions = interactions.values("channel", "direction").annotate(count=Count('channel'))
@@ -45,8 +45,8 @@ def interact(request,cid):
 
     if request.method == "POST":
         customer = Customer.objects.get(id=cid)
-        channels = request.POST["channel"]
-        directions = request.POST["direction"]
+        channel = request.POST["channel"]
+        direction = request.POST["direction"]
         summary = request.POST["summary"]
         interaction = Interaction.objects.create(
             customer=customer,
