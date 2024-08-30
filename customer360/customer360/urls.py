6783c88a658301048cd2cbed6dd33de5b2964ec3
HarbySmart <155396836+HarbySmart@users.noreply.github.com>
2024-08-31 00:38:27 +0100

    Update urls.py

--- customer360/customer360/urls.py
+++ customer360/customer360/urls.py
@@ -22,6 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.index,name="index"),
     path('create/',views.create_customer,name='create_customer'),
-    path('interact/<int:cid',views.interact,name='interact'),
+    path('interact/<int:cid>',views.interact,name='interact'),
     path('summary/',views.summary,name='summary'),
 ]
